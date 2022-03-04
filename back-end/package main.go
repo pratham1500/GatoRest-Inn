b27fa69719e9cec9f213f79ef17955a389440e40
pratham1500 <pratham.1500@gmail.com>
2022-03-03 20:05:55 -0500

    common backend for all forms

--- back-end/package main.go
+++ back-end/package main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-)
+  )
 
-type Admin struct {
-	gorm.Model
-	Code     string
-	Password string
-}
+     db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+
+  type User struct {
+    gorm.Model
+    UserId  string
+    Password string
+  }
+  Admin := Admin{UserId: "ABC123", Password:XYZ123@}
+
+result := db.Create(&user) // pass pointer of data to Create
+
+user.ID             // returns inserted data's primary key
+result.Error        // returns error
+result.RowsAffected // returns inserted records count
